a7916e127994f674114a977a17aa0c74dead2ccc
Cesar Bautista <cesarebaguilar@gmail.com>
2022-03-17 13:49:03 -0600

    mock

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import router from './routes/Routes'
 import Constans from './commons/Constans'
-import { createConnection } from './commons/Connection'
 
 const app = express()
 const PORT = Constans.PORT
@@ -11,12 +10,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(`/${Constans.CONTEXT_NAME}/${Constans.VERSION}/`, router)
 
-createConnection().then(() => {
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(
       `Example app listening at http://localhost:${PORT}/${Constans.CONTEXT_NAME}/${Constans.VERSION}/`
     )
   })
-})
 
 module.exports = app
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,14 +1,10 @@
 import express from 'express'
 import { HelloController } from '../controller/Health.Controller'
-import { CrudController } from '../controller/Crud.Controller'
+import { simulacionController } from '../controller/simulacion.controller'
 
 const router = express.Router()
 
 router.get('/', HelloController.healthCheck)
-
-router
-  .route('/crud')
-  .post(CrudController.saveCrud)
-  .get(HelloController.healthCheck)
+router.get('/simulacion', simulacionController.simulacion)
 
 module.exports = router
