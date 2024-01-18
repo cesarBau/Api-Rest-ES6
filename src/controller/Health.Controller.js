189c06be7e993bb264ec5c9b5ff2cbd0fd61dd16
Cesar Bautista <68753523+cesarBau@users.noreply.github.com>
2024-01-18 16:02:55 -0600

    a ver que pasa
    Hola mundo

--- src/controller/Health.Controller.js
+++ src/controller/Health.Controller.js
@@ -7,8 +7,47 @@ const healthCheck = async (req, res) => {
   console.log('method healthCheck Controller ending')
   res.json(result)
 }
+
+const errorResponse = async (req, res) => {
+  console.log('method errorResponse Controller started')
+  const result = {
+    'confirmation-id': null,
+    reason: {
+      code: 'failure',
+      'error-key': 'InternalError',
+      retriable: false,
+      'error-message': 'Predefined error'
+    }
+  }
+  console.log(`result => ${JSON.stringify(result)}`)
+  console.log('method errorResponse Controller ending')
+  res.json(result)
+}
+
+const correctResponse = async (req, res) => {
+  console.log('method correctResponse Controller started')
+  const result = {
+    'confirmation-id': 914567181919,
+    'partner-request-id': 1234567890,
+    'activation-data': {
+      deeplink: 'https://tv.apple.com/us/subscription/link?product=PPCC_ATV_HB&id=1472441559&token=eyJhbGciOiJIUzI1NiJ9',
+      expiration: '2022-11-22 04:44:16'
+    },
+    reason: {
+      code: 'success',
+      retriable: false
+    }
+  }
+  console.log(`result => ${JSON.stringify(result)}`)
+  console.log('method correctResponse Controller ending')
+  res.json(result)
+}
+
+
 export const HelloController = {
   healthCheck,
+  errorResponse,
+  correctResponse
 }
 
 export default null
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,13 +1,11 @@
 import express from 'express'
 import { HelloController } from '../controller/Health.Controller'
-import { CrudController } from '../controller/Crud.Controller'
 
 const router = express.Router()
 
 router.get('/', HelloController.healthCheck)
+router.post('/god/entitlement/create', HelloController.correctResponse)
+router.post('/bad/entitlement/create', HelloController.errorResponse)
 
-router.route('/crud').post(CrudController.saveCrud).get(CrudController.getCrud)
-
-router.route('/crud/:id').put(CrudController.updateCrud).delete(CrudController.deleteCrud).get(CrudController.getByIdCrud)
 
 module.exports = router
